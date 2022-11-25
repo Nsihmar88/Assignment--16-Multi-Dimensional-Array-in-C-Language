120f8121fb138ff7ae41164976ce82348c20e1dd
Naresh Singh Sihmar <111731321+Nsihmar88@users.noreply.github.com>
2022-11-25 13:49:01 +0530

    Update A16_Prob_10.c
    entire code is changed to get the desired result.

--- A16_Prob_10.c
+++ A16_Prob_10.c
@@ -4,7 +4,7 @@
 
 int main()
 {
-    int matrix_1[10][10], Array_1[100],row, column, i , j, count=0, result;
+    int row, column, i , j, count=0, result=-1;
 
     printf("\nFor order of matrix:");
     printf("\nEnter number of row in matrix: ");
@@ -13,6 +13,7 @@ int main()
     scanf("%d",&column);
     printf("Order of matrix is: %d X %d.", row, column);
 
+    int matrix_1[row][column], Array_1[row];
     // matrix input
     printf("\n\nEnter elements of matrix: \n");
     for (i = 0; i < row; i++)
@@ -25,6 +26,7 @@ int main()
 
     for (i = 0; i < row; i++)
     {
+        count=0;
         for (j = 0; j < column; j++)
         {
             if (matrix_1[i][j]==1)
@@ -37,14 +39,22 @@ int main()
     {
         for (j = 1; j < row; j++)
         {
-            if (Array_1[i]>Array_1[j])
+            if (Array_1[i]!=0)
+            {
+                if (Array_1[i]>Array_1[j])
                 result = i;
-            else
+                else
                 result = j;
+            }
         }
     }
     // Printing result
-    printf("\nMatrix row has maximum number of 1s is: %d row.",result);
+    if (result=0)
+    {
+        printf("\nMatrix row has maximum number of 1s is: row %d.",result+1);
+    }
+    else
+        printf("\nMatrix don't have one in any row.");
     printf("\n ");
     return 0;
 }
